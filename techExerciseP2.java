a4b2e83d2ada43ba27345ccf5e4b7a02aa30fde8
Roberto Saavedra <robertosaavedra@unomaha.edu>
2023-06-09 17:12:53 -0500

    woo!

---
+++ techExerciseP2.java
@@ -0,0 +1,12 @@
+/*
+"Hello World" tutorial
+
+for 'Circuit' group
+*/
+
+public class techExerciseP2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!@!!");
+        System.out.println();
+    }
+}
