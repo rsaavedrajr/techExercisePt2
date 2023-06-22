45798e7654c24bcbe3f911a5b0893b48adc3855d
mcm-ryan <mcm.ryan02@gmail.com>
2023-06-22 18:52:16 -0500

    These are my changes to the hello world program.

--- techExerciseP2.java
+++ techExerciseP2.java
@@ -6,7 +6,7 @@ for 'Circuit' group
 
 public class techExerciseP2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!@!!");
+        System.out.println("Hello World My Name Is Ryan!!");
         System.out.println();
     }
 }
