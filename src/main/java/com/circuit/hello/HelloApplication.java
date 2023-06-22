60062b7e093b896cc277ddb3b71dfd31487fedc3
Elias <eliasmendyk@gmail.com>
2023-06-22 18:46:32 -0500

    EliasMendyk

--- src/main/java/com/circuit/hello/HelloApplication.java
+++ src/main/java/com/circuit/hello/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication {
     @RequestMapping("/test")
     
     String home() {
-        return "Hello World!!!@!!";
+        return "Minecraft is OP";
     }
 
 	public static void main(String[] args) {
