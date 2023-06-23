4fecc5fa5aac2b109e05acbc2d8e2d78ff55c36f
Roberto Saavedra <robertosaavedra@unomaha.edu>
2023-06-22 19:03:30 -0500

    undo test

---
+++ src/main/java/com/circuit/hello/HelloApplication.java
@@ -0,0 +1,21 @@
+package com.circuit.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloApplication {
+
+    @RequestMapping("/test")
+    
+    String home() {
+        return "Hello World!!!@!!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+}
